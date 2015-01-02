447801a8afc83a0c2e3da52666faaea5254c32a5
billybob25 <anthony.h.hornick@gmail.com>
2015-01-02 18:59:13 -0500

    nyan cat

---
+++ dr.py
@@ -0,0 +1,10 @@
+import random
+
+import time
+
+while True:
+    print('Nyan Cat',end=' ')
+    
+    
+
+    
---
+++ nyan cat.py
@@ -0,0 +1,10 @@
+import time
+for count5 in range(4):
+    for count in range(12):
+        print('badgers')
+        time.sleep(0.3) 
+    for count in range(2):
+        print('mushroom')
+        time.sleep(0.2)
+for count2 in range(2):
+    print('snake!!!!!')
